615b37a51065fb8deed6681f14c7dbe9aa486fc2
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-21 13:24:24 -0400

    commit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,30 +11,37 @@ var mysql = require("mysql");
 
 // connection.connect(function (err) {
 //     if (err) throw err;
-//     productInfo();
+//     orderInfo();
 // });
 
 var orderIdNumber;
-    var orderUnits;
+var orderUnits;
 
 function orderInfo() {
     inquirer.prompt([{
-                name: "productID",
-                type: "input",
-                message: "What is the ID of the product you want to buy?"
-            },
-            {
-                name: "units",
-                type: "input",
-                message: "How many units of the product do you want to buy?"
-            }
+            name: "productID",
+            type: "input",
+            message: "What is the ID of the product you want to buy?"
+        },
+        {
+            name: "units",
+            type: "input",
+            message: "How many units of the product do you want to buy?"
+        }
     ]).then(function (order) {
-            orderIdNumber = order.productID;
-            orderUnits = order.units;
-            console.log("You have placed an order for " + orderUnits + " unit(s) of item number " + orderIdNumber + ".");
-        });
+        orderIdNumber = order.productID;
+        orderUnits = order.units;
+        console.log("You have placed an order for " + orderUnits + " unit(s) of item number " + orderIdNumber + ".");
+    });
+}
+
+function processOrder() {
+    if (orderUnits < stock_quantity) {
+        console.log("Sorry! There aren't enough units in stock to process your order. Please try again.")
+        orderInfo()
+    }
 }
 
 orderInfo();
 
-
+processOrder();

a393a79892fc192eccbb19bb6af06a5677f98c2c
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-21 13:52:47 -0400

    commit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,18 +1,18 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 8889,
-//     user: "root",
-//     password: "root",
-//     database: "bamazon"
-// });
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "bamazon"
+});
 
-// connection.connect(function (err) {
-//     if (err) throw err;
-//     orderInfo();
-// });
+connection.connect(function (err) {
+    if (err) throw err;
+    orderInfo();
+});
 
 var orderIdNumber;
 var orderUnits;
@@ -36,10 +36,20 @@ function orderInfo() {
 }
 
 function processOrder() {
-    if (orderUnits < stock_quantity) {
-        console.log("Sorry! There aren't enough units in stock to process your order. Please try again.")
-        orderInfo()
-    }
+    var query ="SELECT id, stock_quantity FROM products ?,";
+    connection.query(query, [order.productID, order.units], function(err,res) {
+        if (err) throw err;
+        if (order.productID === res.id &&  order.units > res.stock_quantity) {
+            console.log("Sorry! There aren't enough units in stock to process your order. Please try again.")
+            orderInfo()
+        } else if (order.productID === res.id && order.units <= res.stock_quantity) {
+            //If the ID the user inputs matches an ID in the DB that has a stock quantity
+            //that's bigger than or the same as the amount of units the user wants: 
+                //1. Update the DB to reflect the stuff the user ordered
+                //2. Store order.units * res.price  in a totalCost variable
+                //3. Print this value for the user
+        }
+    });
 }
 
 orderInfo();

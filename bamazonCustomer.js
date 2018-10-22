a663eb080b8edb946b2f18e4f08992fdd41b44b6
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-22 14:40:02 -0400

    Commit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,19 +20,17 @@ function read() {
     connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     console.log(res);
-    connection.end();
     orderInfo();
     });
 }
-
-
 
 var orderIdNumber;
 var orderUnits;
 
 function orderInfo() {
     
-    inquirer.prompt([{
+    inquirer.prompt([
+        {
             name: "productID",
             type: "input",
             message: "What is the ID of the product you want to buy?"
@@ -43,29 +41,31 @@ function orderInfo() {
             message: "How many units of the product do you want to buy?"
         }
     ]).then(function (order) {
-        orderIdNumber = order.productID;
-        orderUnits = order.units;
-        console.log("You have placed an order for " + orderUnits + " unit(s) of item number " + orderIdNumber + ".");
-    });
-    processOrder();
+        var query = "SELECT id, stock_quantity FROM products WHERE ?";
+        connection.query(query, {id: order.productID}, function(err, res) {
+            if (err) throw err;
+            console.log(res);
+            
+        });
+   });
 }
 
-function processOrder() {
-    var query ="SELECT id, stock_quantity FROM products";
-    console.log(query);
-    connection.query(query, {id: orderIdNumber, stock_quantity: orderUnits}, function(err,res) {
-        console.log(res);
-        if (err) throw err;
-        if (orderIdNumber === res.id &&  orderUnits > res.stock_quantity) {
-            console.log("Sorry! There aren't enough units in stock to process your order. Please try again.")
-            orderInfo()
-        } else if (orderIdNumber === res.id && orderUnits <= res.stock_quantity) {
-            console.log("Your order was successful.");
+// function processOrder() {
+//     var query ="SELECT id, stock_quantity FROM products";
+//     console.log(query);
+//     connection.query(query, {id: orderIdNumber, stock_quantity: orderUnits}, function(err,res) {
+//         console.log(res);
+//         if (err) throw err;
+//         if (orderIdNumber === res.id &&  orderUnits > res.stock_quantity) {
+//             console.log("Sorry! There aren't enough units in stock to process your order. Please try again.")
+//             orderInfo()
+//         } else if (orderIdNumber === res.id && orderUnits <= res.stock_quantity) {
+//             console.log("Your order was successful.");
 //             // If the ID the user inputs matches an ID in the DB that has a stock quantity
 //             // that's bigger than or the same as the amount of units the user wants: 
 //             //     1. Update the DB to reflect the stuff the user ordered
 //             //     2. Store order.units * res.price  in a totalCost variable
 //             //     3. Print this value for the user
-        }
-    });
-}
+//         }
+//     });
+// }

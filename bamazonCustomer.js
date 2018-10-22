28dd96110e642a8c8690313947504f59f1354d99
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-22 15:23:44 -0400

    commit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,9 +24,6 @@ function read() {
     });
 }
 
-var orderIdNumber;
-var orderUnits;
-
 function orderInfo() {
     
     inquirer.prompt([
@@ -41,31 +38,14 @@ function orderInfo() {
             message: "How many units of the product do you want to buy?"
         }
     ]).then(function (order) {
-        var query = "SELECT id, stock_quantity FROM products WHERE ?";
+        var requestedAmount;
+        var query = "SELECT stock_quantity FROM products WHERE ?";
         connection.query(query, {id: order.productID}, function(err, res) {
             if (err) throw err;
-            console.log(res);
-            
+            for (var i = 0; i < res.length; i++) {
+                requestedAmount = res[i].stock_quantity;
+                console.log(requestedAmount);
+            }
         });
    });
 }
-
-// function processOrder() {
-//     var query ="SELECT id, stock_quantity FROM products";
-//     console.log(query);
-//     connection.query(query, {id: orderIdNumber, stock_quantity: orderUnits}, function(err,res) {
-//         console.log(res);
-//         if (err) throw err;
-//         if (orderIdNumber === res.id &&  orderUnits > res.stock_quantity) {
-//             console.log("Sorry! There aren't enough units in stock to process your order. Please try again.")
-//             orderInfo()
-//         } else if (orderIdNumber === res.id && orderUnits <= res.stock_quantity) {
-//             console.log("Your order was successful.");
-//             // If the ID the user inputs matches an ID in the DB that has a stock quantity
-//             // that's bigger than or the same as the amount of units the user wants: 
-//             //     1. Update the DB to reflect the stuff the user ordered
-//             //     2. Store order.units * res.price  in a totalCost variable
-//             //     3. Print this value for the user
-//         }
-//     });
-// }

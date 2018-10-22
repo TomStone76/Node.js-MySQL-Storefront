19b92b6d548f19f8efb6944e2cfc00d42e4314d9
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-22 11:57:56 -0400

    commit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -27,8 +27,8 @@ function read() {
 
 
 
-// var orderIdNumber;
-// var orderUnits;
+var orderIdNumber;
+var orderUnits;
 
 function orderInfo() {
     
@@ -47,27 +47,25 @@ function orderInfo() {
         orderUnits = order.units;
         console.log("You have placed an order for " + orderUnits + " unit(s) of item number " + orderIdNumber + ".");
     });
-
+    processOrder();
 }
-// function processOrder() {
-//     var query ="SELECT id, stock_quantity FROM products ?,";
-//     connection.query(query, {id: order.productID, stock_quantity: order.units}, function(err,res) {
-//         console.log(res);
-//         if (err) throw err;
-//         if (order.productID === res.id &&  order.units > res.stock_quantity) {
-//             console.log("Sorry! There aren't enough units in stock to process your order. Please try again.")
-//             orderInfo()
-//         } else if (order.productID === res.id && order.units <= res.stock_quantity) {
-//             console.log("Your order was successful.");
-// //             // If the ID the user inputs matches an ID in the DB that has a stock quantity
-// //             // that's bigger than or the same as the amount of units the user wants: 
-// //             //     1. Update the DB to reflect the stuff the user ordered
-// //             //     2. Store order.units * res.price  in a totalCost variable
-// //             //     3. Print this value for the user
-//         }
-//     });
-// }
 
-// // orderInfo();
-
-// processOrder();
+function processOrder() {
+    var query ="SELECT id, stock_quantity FROM products";
+    console.log(query);
+    connection.query(query, {id: orderIdNumber, stock_quantity: orderUnits}, function(err,res) {
+        console.log(res);
+        if (err) throw err;
+        if (orderIdNumber === res.id &&  orderUnits > res.stock_quantity) {
+            console.log("Sorry! There aren't enough units in stock to process your order. Please try again.")
+            orderInfo()
+        } else if (orderIdNumber === res.id && orderUnits <= res.stock_quantity) {
+            console.log("Your order was successful.");
+//             // If the ID the user inputs matches an ID in the DB that has a stock quantity
+//             // that's bigger than or the same as the amount of units the user wants: 
+//             //     1. Update the DB to reflect the stuff the user ordered
+//             //     2. Store order.units * res.price  in a totalCost variable
+//             //     3. Print this value for the user
+        }
+    });
+}
